0dc7e11f7228917a636d57c32876db0c24fff69e
Robert <viniolli@gmail.com>
2018-10-31 21:02:56 +0100

    Integration testing

--- testing2/src/components/CommentBox.js
+++ testing2/src/components/CommentBox.js
@@ -7,7 +7,7 @@ class CommentBox extends Component {
 
   state = { comment: '' };
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({ comment: event.target.value });
   };
 
@@ -21,16 +21,19 @@ class CommentBox extends Component {
 
   render() {
     return(
-      <form onSubmit={this.handleSubmit}>
-        <h4>Add comment</h4>
-        <textarea
-          onChange={this.handleChange}
-          value={this.state.comment}
-        />
-        <div>
-          <button>Submit Comment</button>
-        </div>
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add comment</h4>
+          <textarea
+            onChange={this.handleChange}
+            value={this.state.comment}
+            />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button className="fetch-comments" onClick={this.props.fetchComments}>Fetch Comments</button>
+      </div>
     );
   }
 }
